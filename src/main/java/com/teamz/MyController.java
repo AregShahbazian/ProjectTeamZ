261e9a0d0e27dcc9f47820e07c6587ade95cd112
Harriet Mulder <harriet.mulder@youngcolfield.nl>
2017-01-11 16:16:18 +0100

    update pom

--- src/main/java/com/teamz/MyController.java
+++ src/main/java/com/teamz/MyController.java
@@ -1,5 +1,6 @@
 package com.teamz;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +9,21 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MyController {
 
-	
 	@RequestMapping("quiz")
-	public String post(){
-		
+	public String post() {
+
 		RestTemplate restTemplate = new RestTemplate();
 		String movie = restTemplate.getForObject("http://www.omdbapi.com/?i=tt0120338&plot=short&r=json", String.class);
-		JSONObject jsonObject= new JSONObject(movie);
-		System.out.println(movie.toString());
-		
-		return jsonObject.toString();
+		JSONObject jsonObject;
+		try {
+			jsonObject = new JSONObject(movie);
+			return jsonObject.toString();
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
 	}
 
 }
