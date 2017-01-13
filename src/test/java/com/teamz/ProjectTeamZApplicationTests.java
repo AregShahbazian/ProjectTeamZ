d479a2f0a316deaa31cc1f9375a590b81bdbdc2d
Areg Shahbazian <areg.shahbazian@youngcolfield.nl>
2017-01-13 13:07:51 +0100

    Unit test

--- src/test/java/com/teamz/ProjectTeamZApplicationTests.java
+++ src/test/java/com/teamz/ProjectTeamZApplicationTests.java
@@ -1,19 +1,35 @@
 package com.teamz;
 
+import static org.junit.Assert.assertTrue;
+
+import javax.inject.Inject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
+
+import com.teamz.domain.Movie;
+import com.teamz.repository.MovieRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjectTeamZApplicationTests {
 
+	@Inject
+	private MovieRepository movieRepo;
+
 	@Test
 	public void contextLoads() {
-			
-		
+
+		Movie movie = new Movie();
+		movie.setApiId("fooId");
+
+		int sizeBefore = movieRepo.findAll().size();
+		movieRepo.save(movie);
+		int sizeAfter = movieRepo.findAll().size();
+
+		assertTrue(sizeAfter == sizeBefore + 1);
+
 	}
 }
-
